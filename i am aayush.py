585e5f9734628135cdec4b661499a325fb1adb12
Aayush Chawan <aayushchawan3@gmail.com>
2026-06-28 20:05:14 +0530

    Day 1 python practice

---
+++ i am aayush.py
@@ -0,0 +1,76 @@
+print("learning something new")
+print("i am about to kill you")
+
+your_name= "Aayush the great coder"
+print(f"hello {your_name}")
+food="apple"
+print(f"you like {food}")
+email="aayushchawan3@gmail.com"
+print("my email is {email}")
+#integers
+
+age=12
+a=19
+print(f"you are {age} years old")
+print(f"a+{age}")
+
+#floats
+a=0.79
+b=34.56
+
+print(a+b)
+total=b+a*b
+print(f"Total : {total}")
+
+#boolean
+is_older_than_18 = True
+print(f"is_older_than_18: {is_older_than_18}")
+
+if is_older_than_18:
+    print("You are eligible to watch this")
+else:
+    print("You are not eligible to watch this")
+
+a = True
+if a:
+    print("hi")
+else:
+    print("get out")
+
+is_with_other_boy = True
+if is_with_other_boy:
+    print("SHE IS CHEATING ON YOU")
+else:
+    print("SHE IS NOT CHEATING ON YOU")
+
+#type casting
+my_name = "BOB THE BUILDER"
+age = 25
+weight = 1000.9
+is_your_friend = True
+
+print(type(weight))
+
+weight = int(weight)
+print(f"my weight is :{weight}")
+
+age = str(age)
+print(age)
+
+name=input("What is your name? :")
+print(f"You are the best {name}")
+
+age = input("what is your age?")
+age = int(age)
+age += 1
+
+#can i put such constraints that i can say you are old if above 45 and you are young iif answer is below 45
+print(f"you are {age} years old")
+
+    
+
+
+
+
+
+
